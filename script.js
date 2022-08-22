942394b5083cc8c7d7230322a44cd1f9839c2878
mari <sermarajraj35@gmail.com>
2022-08-22 03:25:51 -0800

    Dom

---
+++ script.js
@@ -0,0 +1,31 @@
+function statup()
+{
+var a=document.getElementById("heading")
+// document.write(a);
+a.style.color="red";
+// a.style.fontSize="80px"
+}
+
+// function validation()
+// {
+//     var fname=document.getElementById("fname")
+//     if(fname==null||fname==""){
+//         alert("Please Enter Your Name")
+//         return false
+//     }
+    
+// }
+
+function passcomp(){
+    let c_pass=document.getElementById("cpass").value;
+    let pass_1=document.getElementById("pass").value;
+    if(c_pass==pass_1){
+        document.getElementById("span1").innerHTML="Your password Is Correct";
+    }
+    else{
+        let z=document.getElementById("span1").innerHTML="Your password Is INcorrect";
+        
+
+    }
+    
+}
